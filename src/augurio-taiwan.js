1e403dbea84a627b0fb79670cfa912d676e02999
auguston <evilkim25@gmail.com>
2019-10-05 21:40:17 +0800

    更新註解

--- src/augurio-taiwan.js
+++ src/augurio-taiwan.js
@@ -6,15 +6,15 @@ const TaiwanMap = new Vue({
   },
   methods: {
     async getTaiwanMap() {
-      var width = (this.$refs.map.offsetWidth).toFixed(),
-          height = (this.$refs.map.offsetHeight).toFixed();
+      const width = (this.$refs.map.offsetWidth).toFixed(),
+            height = (this.$refs.map.offsetHeight).toFixed();
 
       // 判斷螢幕寬度，給不同放大值
-      let mercatorScale;
-      if(window.screen.width > 1366) {
+      let mercatorScale, w = window.screen.width;
+      if(w > 1366) {
         mercatorScale = 11000;
       }
-      else if(window.screen.width <= 1366 && window.screen.width > 480) {
+      else if(w <= 1366 && w > 480) {
         mercatorScale = 9000;
       }
       else {
@@ -30,39 +30,40 @@ const TaiwanMap = new Vue({
           .scale(mercatorScale)
           .translate([width/2, height/2.5])
       );
-
+      
+      // 讓d3抓svg，並寫入寬高
       var svg = await d3.select('#svg')
           .attr('width', width)
           .attr('height', height)
-          .attr('viewBox', `0 0 ${width} ${height}`)
-          .attr('preserveAspectRatio', 'xMidYMid meet');
-
+          .attr('viewBox', `0 0 ${width} ${height}`);
+      
+      // 讓d3抓GeoJSON檔，並寫入path的路徑
       var url = 'dist/taiwan.geojson';
-      await d3.json(url, (error, topology) => {
+      await d3.json(url, (error, geometry) => {
         if (error) throw error;
 
         svg
           .selectAll('path')
-          .data(topology.features)
+          .data(geometry.features)
           .enter().append('path')
           .attr('d', path)
           .attr({
-            fill: '#000',
-            id: (d) => 'city' + d.properties.COUNTYCODE,
-            ch: (d) => d.properties.COUNTYNAME,
-            en: (d) => d.properties.COUNTYENG
+            // 設定id，為了click時加class用
+            id: (d) => 'city' + d.properties.COUNTYCODE
           })
           .on('click', d => {
-            this.h1 = d.properties.COUNTYNAME;
-            this.h2 = d.properties.COUNTYENG;
+            this.h1 = d.properties.COUNTYNAME; // 換中文名
+            this.h2 = d.properties.COUNTYENG; // 換英文名
+            // 有 .active 存在，就移除 .active
             if(document.querySelector('.active')) {
               document.querySelector('.active').classList.remove('active');
             }
+            // 被點擊的縣市加上 .active
             document.getElementById('city' + d.properties.COUNTYCODE).classList.add('active');
           });
       });
       return svg;
-    }
+    },
   },
   mounted() {
 
